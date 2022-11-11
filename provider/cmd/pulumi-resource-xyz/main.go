9a18cfe4a54bdbae3d01c7549d9874bd95784ef0
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2022-11-11 15:21:17 +0530

    Update providerName with placeholder

--- provider/cmd/pulumi-resource-xyz/main.go
+++ provider/cmd/pulumi-resource-xyz/main.go
@@ -9,7 +9,7 @@ import (
 	"github.com/cloudy-sky-software/pulumi-provider-template/provider/pkg/version"
 )
 
-var providerName = "render"
+var providerName = "xyz"
 
 //go:embed schema.json
 var pulumiSchema []byte
