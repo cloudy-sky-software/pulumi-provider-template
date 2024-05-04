7f6e702af608a9bb9b5d932569b5c33126e6920b
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-05-04 12:00:07 -0700

    Update schema.go

--- provider/pkg/gen/schema.go
+++ provider/pkg/gen/schema.go
@@ -94,7 +94,7 @@ func PulumiSchema(openapiDoc openapi3.T) (pschema.PackageSpec, openapigen.Provid
 		ExcludedPaths: []string{},
 	}
 
-	providerMetadata, err := openAPICtx.GatherResourcesFromAPI(csharpNamespaces)
+	providerMetadata, updatedOpenAPIDoc, err := openAPICtx.GatherResourcesFromAPI(csharpNamespaces)
 	if err != nil {
 		contract.Failf("generating resources from OpenAPI spec: %v", err)
 	}
@@ -122,21 +122,6 @@ func PulumiSchema(openapiDoc openapi3.T) (pschema.PackageSpec, openapigen.Provid
 	})
 	pkg.Language["nodejs"] = rawMessage(map[string]interface{}{
 		"packageName": "@cloudyskysoftware/pulumi-xyz",
-		"dependencies": map[string]string{
-			"@pulumi/pulumi":    "^3.0.0",
-			"shell-quote":       "^1.6.1",
-			"tmp":               "^0.0.33",
-			"@types/tmp":        "^0.0.33",
-			"glob":              "^7.1.2",
-			"@types/glob":       "^5.0.35",
-			"node-fetch":        "^2.3.0",
-			"@types/node-fetch": "^2.1.4",
-		},
-		"devDependencies": map[string]string{
-			"mocha":              "^5.2.0",
-			"@types/mocha":       "^5.2.5",
-			"@types/shell-quote": "^1.6.0",
-		},
 	})
 	pkg.Language["python"] = rawMessage(map[string]interface{}{
 		"packageName": "pulumi_xyz",
@@ -148,8 +133,11 @@ func PulumiSchema(openapiDoc openapi3.T) (pschema.PackageSpec, openapigen.Provid
 	metadata := openapigen.ProviderMetadata{
 		ResourceCRUDMap: providerMetadata.ResourceCRUDMap,
 		AutoNameMap:     providerMetadata.AutoNameMap,
+		SDKToAPINameMap:  providerMetadata.SDKToAPINameMap,
+		APIToSDKNameMap:  providerMetadata.APIToSDKNameMap,
+		PathParamNameMap: providerMetadata.PathParamNameMap,
 	}
-	return pkg, metadata
+	return pkg, metadata, updatedOpenAPIDoc
 }
 
 func rawMessage(v interface{}) pschema.RawMessage {
