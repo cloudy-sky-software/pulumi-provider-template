d53ed4e4270870d646dcd9518505453d3b31b633
Praneet Loke <1466314+praneetloke@users.noreply.github.com>
2024-05-04 12:09:23 -0700

    Create openapi_fixes.go

---
+++ provider/pkg/gen/openapi_fixes.go
@@ -0,0 +1,12 @@
+package gen
+
+import (
+	"github.com/getkin/kin-openapi/openapi3"
+)
+
+// FixOpenAPIDoc applies patches to the raw OpenAPI spec
+// before passing it to pulschema.
+func FixOpenAPIDoc(openAPIDoc *openapi3.T) error {
+	// TODO: Fill me in if there are patches to be made!
+	return nil
+}
